25cfb9dfc5b4807ad140facb724ee78fc99785bf
Robertas Misevičius <robertas31810@gmail.com>
2022-10-17 15:33:41 +0300

    first

---
+++ js/numbers.js
@@ -0,0 +1,28 @@
+'use strict';
+console.log('numbers.js');
+
+
+
+let numbersArray =[2, 12, 33, -7, 28, 49, -17, 19, 42, -14, -10, 20];
+
+// kur issaugosim dvigubas reiksmes
+const doubleArray = [];
+for (let i = 0; i < numbersArray.length; i++) {
+    let nr = numbersArray[i];
+    let double = nr * 2;
+    console.log(double);
+    doubleArray.push(double);
+    
+}
+console.log(doubleArray);
+
+// 22. Suskaiciuti bedra masyvo skaiciu suma
+
+
+
+// 23. suskaiciuoti vidurki
+
+
+
+
+// 24. Atrinkti didziausia masyvo skaiciu
